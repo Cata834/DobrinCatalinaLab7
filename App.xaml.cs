c07f68fae823b0830d04795410dd50eb83ff57c7
Cata <cg850069@gmail.com>
2024-11-27 19:40:51 +0200

    Modificari Laborator 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace DobrinCatalinaLab7
+﻿using System;
+using DobrinCatalinaLab7.Data;
+using System.IO;
+namespace DobrinCatalinaLab7
 {
+
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
