f9d5ea155887c07f75b6ceb429ab633bfe546773
Cata <cg850069@gmail.com>
2024-12-07 20:11:56 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -8,16 +8,74 @@ public partial class ListPage : ContentPage
 		InitializeComponent();
 	}
     async void OnSaveButtonClicked(object sender, EventArgs e)
+{
+    var shoplist = (ShopList)BindingContext;
+    if (!string.IsNullOrWhiteSpace(shoplist.Description))
     {
-        var slist = (ShopList)BindingContext;
-        slist.Date = DateTime.UtcNow;
-        await App.Database.SaveShopListAsync(slist);
+        await App.Database.SaveShopListAsync(shoplist);
         await Navigation.PopAsync();
     }
-    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    else
     {
-        var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
-        await Navigation.PopAsync();
+        await DisplayAlert("Error", "Please enter a description.", "OK");
     }
 }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        // Navigãm la ProductPage, având în vedere cã BindingContext este un obiect ShopList
+        var shoplist = (ShopList)BindingContext;
+
+        // Deschidem ProductPage ?i îi setãm BindingContext pentru a adãuga produse
+        await Navigation.PushAsync(new ProductPage(shoplist));
+    }
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        // Verificãm dacã existã un element selectat în ListView
+        var selectedProduct = listView.SelectedItem as Product;
+
+        if (selectedProduct != null)
+        {
+            // ?tergem produsul selectat
+            await App.Database.DeleteProductAsync(selectedProduct);
+
+            // Actualizãm lista
+            var shopl = (ShopList)BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+        else
+        {
+            // Afi?ãm o eroare dacã nu s-a selectat nimic
+            await DisplayAlert("Error", "Please select a product to delete", "OK");
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        
+        var selectedProduct = listView.SelectedItem as Product;
+
+        if (selectedProduct != null)
+        {
+            
+            await App.Database.DeleteProductAsync(selectedProduct);
+            
+            var shopl = (ShopList)BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+        else
+        {
+            
+            await DisplayAlert("Error", "Please select a product to delete", "OK");
+        }
+    }
+
+}
+
---
+++ ValidationBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Maui.Controls;  
+using Microsoft.Maui.Controls.Xaml;  
+using Microsoft.Maui.Graphics;  
+
+namespace DobrinCatalinaLab7
+{
+    
+    class ValidationBehaviour : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor =
+                string.IsNullOrEmpty(args.NewTextValue) ? Color.FromRgba("#AA4A44") : Color.FromRgba("#FFFFFF");
+        }
+    }
+}
